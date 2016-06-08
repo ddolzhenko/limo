24231a6f00b7e766f7d3304a27d1d2b176b58f5d
Dmitry Dolzhenko <dddolzhenko@luxoft.com>
2016-06-08 17:58:26 +0300

    ltest example stub

---
+++ limo/test.hpp
@@ -0,0 +1,212 @@
+/******************************************************************************
+
+Copyright (c) Dmitri Dolzhenko
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*******************************************************************************/
+
+#pragma once
+
+//------------------------------------------------------------------------------
+
+// include local:
+
+// include std:
+#include <vector>
+#include <list>
+#include <set>
+
+#include <cassert>
+#include <string>
+#include <map>          // Tests
+#include <algorithm>    // std::find
+#include <functional>   // std::fucntion
+
+#include <iostream>
+#include <iomanip>
+
+// forward declarations:
+
+// dummy warnings:
+#pragma GCC diagnostic ignored "-Wunused-variable"
+
+//------------------------------------------------------------------------------
+
+namespace limo {
+
+    class TestContext;
+    
+
+    typedef std::string Name;
+    typedef std::function<void (TestContext&)>    TestFunction;
+    typedef std::function<void(void)>             PrepareFunction;
+    typedef std::map<Name, TestFunction> Tests;
+
+    struct TestSettings
+    {
+        TestSettings(Name name, TestContext* suite): m_name(name), m_context(suite) {}
+        
+        TestSettings& operator<<(TestFunction test) {
+            m_test = test;
+            return *this;
+        }
+
+        Name            m_name;
+        TestContext*    m_context;
+        TestFunction    m_test;
+    };
+
+
+    struct Run
+    {
+        const char* expected;
+        const char* recieved;
+        const char* file;
+        const int line;
+        const bool ok;
+    };
+
+    std::ostream& operator<<(std::ostream& o, const Run& run)
+    {
+        const char tab[] = "    ";
+        return o    << run.file << ":" << run.line << ":: " 
+                    << (run.ok ? "ok" : "failed") << ": "
+                    << tab << run.expected << std::endl;
+                    // << tab << run.recieved << std::endl;
+    };
+
+
+    class TestContext
+    {
+    public:
+        static const bool verbose = true;
+        
+        PrepareFunction m_before;
+        PrepareFunction m_after;
+
+        Name  m_name;
+        Tests m_tests;
+
+        bool m_global;
+
+    public:
+        TestContext(Name name, bool global=false)
+        : m_name(name)
+        , m_global(global)
+        {
+            m_before = [](){};
+            m_after  = [](){};
+
+            // if(!m_global && verbose) std::cout << m_name << std::endl;
+        }
+
+        void run(Name basename="")
+        {
+            for(const auto& test : m_tests)
+            {
+                run_test(test.first, test.second, basename);
+            }
+        }
+
+        void add(Name name, TestFunction test)
+        {
+            m_tests[name] = test;
+
+            if(!m_global)
+            {
+                run_test(name, test, m_name+".");
+            }
+        }
+
+
+        void run_test(Name name, TestFunction test, Name basename)
+        {
+            m_before();
+            
+            TestContext context(basename + name);
+            test(context);
+            // results[fullname] = info;
+            m_after();
+            
+            // context.run(context.m_name + ".");
+        }
+
+    public: // outcomes
+        void expect_true(const char* file, int line, const char* expected, bool ok)
+        {
+            Run run = {expected, ok ? "true" : "false", file, line, ok};
+            // db.push_back(run);
+            if(!ok) std::cout << run;
+        }
+
+    private:
+       
+    };
+
+
+
+    struct Registrator
+    {
+        Registrator(const TestSettings& w) {
+            w.m_context->add(w.m_name, w.m_test);
+        }
+    };
+
+
+
+
+    #define LTEST(test_name) \
+        limo::Registrator ltest_ ## test_name = \
+            limo::TestSettings(#test_name,  &ltest_context__) << \
+            [&](limo::TestContext& ltest_context__)
+
+
+    #define LBEFORE limo_context__.m_before = [&]()
+    #define LAFTER  limo_context__.m_after  = [&]()
+
+
+    // Unary
+    #define EXPECT_TRUE(expr)       ltest_context__.expect_true(__FILE__, __LINE__, #expr, expr)
+    #define EXPECT_FALSE(expr)      EXPECT_TRUE(!(expr))
+
+    // Binary
+    #define EXPECT_EQ(expr1, expr2) EXPECT_TRUE((expr1) == (expr2))
+    #define EXPECT_NE(expr1, expr2) EXPECT_TRUE((expr1) != (expr2))
+    #define EXPECT_LT(expr1, expr2) EXPECT_TRUE((expr1) < (expr2))
+    #define EXPECT_LE(expr1, expr2) EXPECT_TRUE((expr1) <= (expr2))
+    #define EXPECT_GT(expr1, expr2) EXPECT_TRUE((expr1) > (expr2))
+    #define EXPECT_GE(expr1, expr2) EXPECT_TRUE((expr1) >= (expr2))
+
+    // Complex
+    template <class T1, class T2>
+    bool in(const std::initializer_list<T1>& s, const T2& x) {
+        return std::find(s.begin(), s.end(), x) != s.end();
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+
+} // namespace limo
+
+//------------------------------------------------------------------------------
+// globals
+
+extern limo::TestContext ltest_context__("root", true);
+
+//------------------------------------------------------------------------------
---
+++ limo_examples/testing/basics/test_basics.cpp
@@ -0,0 +1,107 @@
+/******************************************************************************
+
+Copyright (c) Dmitri Dolzhenko
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*******************************************************************************/
+
+// #include "test_basics.hpp"
+
+//------------------------------------------------------------------------------
+
+// include local:
+#include "limo/test_main.hpp"
+
+// include std:
+
+// forward declarations:
+
+
+//------------------------------------------------------------------------------
+
+// + all macroses
+// + __file__ ___line__
+// + setup/teardown  anyway
+// - print calculated values (if possible)
+// - statistics
+// - context:     CONTEXT(sort, {selection_sort, naive_sort})
+// - select tests to run test_context__.run("search.*")
+// - separate printers and config
+
+// 0.5: 
+// - separate process for each test
+// - perfomance testing parameters
+
+
+int my_sqare(int x)
+{
+    if (x == 3)
+        return 3;
+    return x*x;
+}
+
+
+LTEST(my_sqare) {
+
+    using namespace ltest;
+
+    EXPECT_TRUE(2==2);
+    EXPECT_FALSE(2==3);
+
+    EXPECT_TRUE(2==2);
+    EXPECT_LT(2, 2);
+
+    EXPECT_TRUE(in({1, 2} , 42));
+    EXPECT_TRUE(in({1, 2} , 2));
+
+    EXPECT_EQ(1, 1);
+
+    // auto my_sqare = [](int x) { return x * x; };
+    
+    LTEST(degenarated) {
+        EXPECT_EQ(0, my_sqare(0));
+    };
+    LTEST(trivial) {
+        EXPECT_EQ(1, my_sqare(1));
+    };
+        
+    LTEST(even) {
+        LTEST(positive) {
+            EXPECT_EQ(4, my_sqare(2));  
+            EXPECT_EQ(16, my_sqare(4));  
+        };
+        LTEST(negative) {
+            EXPECT_EQ(4, my_sqare(-2));  
+            EXPECT_EQ(16, my_sqare(-4));  
+        };
+    };
+
+
+    LTEST(odd) {
+        LTEST(positive) {
+            EXPECT_EQ(9, my_sqare(3));  
+            EXPECT_EQ(25, my_sqare(5));  
+        };
+        LTEST(negative) {
+            EXPECT_EQ(9, my_sqare(-3));  
+            EXPECT_EQ(25, my_sqare(-5));  
+        };
+    };
+};
